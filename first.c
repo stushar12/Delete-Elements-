14e608ef8ff083d92bb7bdbc0de864a8e9a7cd47
Tushar Saxena <1805718@kiit.ac.in>
2019-07-23 00:15:40 +0530

    changes made successfully

--- first.c
+++ first.c
@@ -1,17 +1,14 @@
 #include<stdio.h>
 int main()
 {
-  int arr[10];
-  int x;
-  int arr2[10];
-  int temp;
+  int arr[10],x,temp;
   printf("\n Enter the number of elements of array ");
   scanf("%d",&x);
   for(int i =0;i<x;i++)
   {
     scanf("%d",&arr[i]);
  }
-  for(int i=0;i<x-1;i++)
+  for(int i=0;i<x-1;i++)  /////Sorting of array
   {
     for(int j=0;j<x-i-1;j++)
     {
@@ -29,14 +26,14 @@ int main()
   {
     if(arr[i]!=arr[i+1])
     {
-      arr2[k]=arr[i];
+      arr[k]=arr[i];
       k++;
     }
   }
-  arr2[k]=arr[x-1];
+  arr[k]=arr[x-1];
 
   printf("\n Array is ");
   for(int i=0;i<=k;i++)
-  printf("%d ",arr2[i]);
+  printf("%d ",arr[i]);
 
 }
